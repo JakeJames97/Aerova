70508b3ffa4fa8f271b19ffe1d0448eee5dc362e
Jake James <jakejames18@virginmedia.com>
2026-06-20 18:03:33 +0100

    chore: protecting trying to convert gpp with gbp

--- app/Http/Resources/DestinationResource.php
+++ app/Http/Resources/DestinationResource.php
@@ -32,7 +32,7 @@ class DestinationResource extends JsonResource
 
     protected function getConvertedToLocalCurrencyBudget(?Country $country, ?int $budget): ?string
     {
-        if (!$budget || !$country) {
+        if (!$budget || !$country || $country->currency === 'GBP') {
             return null;
         }
 
