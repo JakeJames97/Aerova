2a6bf7626e213a44de68c34336b20b39c3670aa1
Jake James <jakejames18@virginmedia.com>
2026-07-15 11:49:00 +0100

    chore: sorting out favicon and some meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,15 @@
 <head>
     <meta charset="utf-8">
     <title>Aerova</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="/favicon.ico" sizes="any" />
+
+    <meta name="description" content="Plan trips, track destinations, and discover where others are travelling. Aerova is a trip planner for people who like a plan." />
+
+    <meta property="og:title" content="Aerova" />
+    <meta property="og:description" content="Plan trips, track destinations, and discover where others are travelling." />
+    <meta property="og:type" content="website" />
+
     @vite(['resources/css/app.scss', 'resources/js/app.js'])
 </head>
 <body>
